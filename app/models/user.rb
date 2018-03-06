0c0d30cc117fc23505a3b789383c2de29bab5cc9
Willis Roberts <willisroberts49@gmail.com>
2018-03-06 14:54:34 -0500

    Complete Backend Auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  after_initialization :ensure_session_token
+  after_initialize :ensure_session_token
 
   def self.find_by_credentials(username, password)
     user = User.find_by(username: username)
