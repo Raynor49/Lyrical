71e0f54fdca8259a795150a893230e93d4192a44
WILLIS <willisroberts49@gmail.com>
2024-03-12 00:22:08 -0400

    remove uglifier

--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,12 @@ var prodPlugins = [
     'process.env': {
       'NODE_ENV': JSON.stringify('production')
     }
-  }),
-  new webpack.optimize.UglifyJsPlugin({
-    compress: {
-      warnings: true
-    }
   })
+  // new webpack.optimize.UglifyJsPlugin({
+  //   compress: {
+  //     warnings: true
+  //   }
+  // })
 ];
 
 plugins = plugins.concat(
